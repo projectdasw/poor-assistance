aa6295c6a161d5d45735f369af53687769ee1991
DASW Project <project.dasw@gmail.com>
2024-10-29 09:50:08 +0700

    Update init.py - Proses pengambilan data payoff setiap ronde dan diacak sebagai final payment

--- pay_random/__init__.py
+++ pay_random/__init__.py
@@ -22,26 +22,23 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
-    # round = models.IntegerField()
-    # potential_payoff = models.FloatField(min=0, max=1, initial=0)
+    round = models.IntegerField()
+    potential_payoff = models.FloatField(min=0, max=1, initial=0)
 
 
 # PAGES
 class Pay_info(Page):
-    pass
-    # @staticmethod
-    # def before_next_page(player: Player, timeout_happened):
-    #     participant = player.participant
-    #     player.potential_payoff = float(participant.ct_payment)
-    #     player.round = int(participant.selected_round)
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        participant = player.participant
+        player.potential_payoff = float(participant.get_payment)
+        player.round = int(participant.selected_round)
+        player.payoff = (player.potential_payoff * 1000) + 10000
 
 
 class Payment(Page):
     pass
-    # @staticmethod
-    # def before_next_page(player: Player, timeout_happened):
-    #     player.payoff = (player.potential_payoff * 50) + 10000
+
 
 
 class End(Page):
