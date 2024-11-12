40cda2e7510bb6d91f191639a68ca7938e7d9606
DASW Project <project.dasw@gmail.com>
2024-11-12 11:31:56 +0700

    membuat program permainan Risky Option Price & Risky Option Allocation

--- practice_experiment/__init__.py
+++ practice_experiment/__init__.py
@@ -1,5 +1,6 @@
 from otree.api import *
 import random
+import json
 from pathlib import Path
 
 doc = """
@@ -15,13 +16,66 @@ def load_word_list():
 class Constants(BaseConstants):
     name_in_url = 'practice_experiment'
     players_per_group = None
-    num_rounds = 3  # 10 periods
+    num_rounds = 3  # 5 periods
     initial_endowment = 100
     additional_endowment = 30
     deduction = 50
+
+    # RISKY OPTION - PRICE
+    options_data_price = [
+        {'name': 'Opsi 1', 'outcomes': [(45, 0.5), (10, 0.25), (0, 0.25)]},
+        {'name': 'Opsi 2', 'outcomes': [(45, 0.4), (20, 0.35), (0, 0.25)]},
+        {'name': 'Opsi 3', 'outcomes': [(45, 0.4), (15, 0.4), (5, 0.2)]},
+        {'name': 'Opsi 4', 'outcomes': [(45, 0.35), (15, 0.55), (10, 0.1)]},
+        {'name': 'Opsi 5', 'outcomes': [(45, 0.45), (10, 0.40), (5, 0.15)]},
+        {'name': 'Opsi 6', 'outcomes': [(40, 0.5), (10, 0.5), (0, 0)]},
+        {'name': 'Opsi 7', 'outcomes': [(40, 0.4), (20, 0.4), (5, 0.2)]},
+        {'name': 'Opsi 8', 'outcomes': [(40, 0.6), (5, 0.2), (0, 0.2)]},
+        {'name': 'Opsi 9', 'outcomes': [(40, 0.45), (20, 0.35), (0, 0.2)]},
+        {'name': 'Opsi 10', 'outcomes': [(40, 0.5), (15, 0.25), (5, 0.25)]},
+        {'name': 'Opsi 11', 'outcomes': [(35, 0.4), (20, 0.4), (15, 0.2)]},
+        {'name': 'Opsi 12', 'outcomes': [(35, 0.5), (25, 0.25), (5, 0.25)]},
+        {'name': 'Opsi 13', 'outcomes': [(35, 0.6), (15, 0.2), (5, 0.5)]},
+        {'name': 'Opsi 14', 'outcomes': [(35, 0.25), (25, 0.65), (0, 0.1)]},
+        {'name': 'Opsi 15', 'outcomes': [(35, 0.5), (20, 0.25), (10, 0.25)]},
+        {'name': 'Opsi 16', 'outcomes': [(30, 0.7), (20, 0.2), (0, 0.1)]},
+        {'name': 'Opsi 17', 'outcomes': [(30, 0.5), (25, 0.4), (0, 0.1)]},
+        {'name': 'Opsi 18', 'outcomes': [(30, 0.6), (20, 0.3), (10, 0.1)]},
+        {'name': 'Opsi 19', 'outcomes': [(30, 0.7), (15, 0.25), (5, 0.05)]},
+        {'name': 'Opsi 20', 'outcomes': [(30, 0.6), (25, 0.25), (5, 0.15)]},
+    ]
+    # RISKY OPTION - PRICE
+
+    # RISKY OPTION - ALLOCATION
+    options_data_allocation = [
+        {'name': 'Opsi 1', 'outcomes': [(1.5, 0.65), (0.25, 0.1), (0, 0.25)]},
+        {'name': 'Opsi 2', 'outcomes': [(1.5, 0.6), (0.5, 0.2), (0, 0.2)]},
+        {'name': 'Opsi 3', 'outcomes': [(1.5, 0.55), (0.75, 0.25), (0, 0.3)]},
+        {'name': 'Opsi 4', 'outcomes': [(1.5, 0.4), (1, 0.4), (0, 0.2)]},
+        {'name': 'Opsi 5', 'outcomes': [(1.5, 0.5), (1.25, 0.2), (0, 0.3)]},
+        {'name': 'Opsi 6', 'outcomes': [(2, 0.4), (0.5, 0.4), (0, 0.2)]},
+        {'name': 'Opsi 7', 'outcomes': [(2, 0.35), (0.75, 0.4), (0, 0.25)]},
+        {'name': 'Opsi 8', 'outcomes': [(2, 0.45), (0.25, 0.4), (0, 0.05)]},
+        {'name': 'Opsi 9', 'outcomes': [(2, 0.4), (1, 0.2), (0, 0.4)]},
+        {'name': 'Opsi 10', 'outcomes': [(2, 0.35), (1.5, 0.2), (0, 0.45)]},
+        {'name': 'Opsi 11', 'outcomes': [(2.5, 0.35), (0.25, 0.5), (0, 0.15)]},
+        {'name': 'Opsi 12', 'outcomes': [(2.5, 0.3), (0.5, 0.5), (0, 0.2)]},
+        {'name': 'Opsi 13', 'outcomes': [(2.5, 0.3), (0.75, 0.35), (0, 0.35)]},
+        {'name': 'Opsi 14', 'outcomes': [(2.5, 0.2), (1, 0.5), (0, 0.3)]},
+        {'name': 'Opsi 15', 'outcomes': [(2.5, 0.1), (1.25, 0.6), (0, 0.3)]},
+        {'name': 'Opsi 16', 'outcomes': [(3, 0.3), (0.5, 0.2), (0, 0.5)]},
+        {'name': 'Opsi 17', 'outcomes': [(3, 0.3), (0.25, 0.4), (0, 0.3)]},
+        {'name': 'Opsi 18', 'outcomes': [(3, 0.2), (1, 0.4), (0, 0.4)]},
+        {'name': 'Opsi 19', 'outcomes': [(3, 0.25), (1.25, 0.25), (0, 0.55)]},
+        {'name': 'Opsi 20', 'outcomes': [(3, 0.15), (0.75, 0.75), (0, 0.1)]},
+    ]
+    # RISKY OPTION - ALLOCATION
+
+    # COGINITIVE TASK
     board_rows = 3  # Jumlah baris papan
     board_columns = 13  # Jumlah kolom papan
     target_character = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')  # Huruf atau angka yang dicari
+    # COGINITIVE TASK
 
 class Subsession(BaseSubsession):
     pass
@@ -32,14 +86,30 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    endowment = models.IntegerField(initial=0)
-    option_price = models.IntegerField(initial=0, label='Jumlah yang ingin di investasikan')
-    option_allocation = models.IntegerField(initial=0, label='Jumlah yang ingin di alokasi')
+    endowment = models.FloatField()
+
+    # RISKY OPTION - PRICE
+    selected_optionprice = models.StringField()
+    result_price = models.FloatField(initial=0)
+    # RISKY OPTION - PRICE
+
+    # RISKY OPTION - ALLOCATION
+    selected_optionallocation = models.StringField()
+    allocation_invest = models.FloatField(initial=0)
+    result_allocation = models.FloatField(initial=0)
+    # RISKY OPTION - ALLOCATION
+
     buy_time = models.IntegerField(initial=0, label='Masukkan jumlah Endowment yang ingin Anda gunakan untuk membeli waktu')
     count_guess = models.IntegerField(label="Berapa kali huruf/angka muncul:")
     actual_count = models.IntegerField(initial=0)
     score = models.IntegerField(initial=0)
     current_target = models.StringField()  # Target huruf/angka yang diacak setiap putaran
+    offer_accepted = models.BooleanField(
+        choices=[
+            (True, 'Iya'),
+            (False, 'Tidak')
+        ]
+    )
 
 
 class Intro(Page):
@@ -65,14 +135,137 @@ class Intro(Page):
         }
 
 
-class Game1(Page):
-    pass
-    # form_model = 'player'
-    # form_fields = ['option_price']
+class BeforeGame1(Page):
+    form_model = 'player'
+    form_fields = ['offer_accepted']
 
 
-class Game2(Page):
-    pass
+class BeforeGame2(Page):
+    form_model = 'player'
+    form_fields = ['offer_accepted']
+
+
+class RiskyOption_Price(Page):
+    form_model = 'player'
+    form_fields = ['selected_optionprice']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        # Mendapatkan pilihan acak (contoh daftar pilihan opsi)
+        random_options = random.sample(Constants.options_data_price, 5)
+
+        # Membuat teks yang terstruktur untuk setiap opsi dalam format list items
+        for i, option in enumerate(random_options):
+            option_outcomes = option['outcomes']
+            formatted_outcomes = []
+            for j, (value, probability) in enumerate(option_outcomes):
+                formatted_outcomes.append(f"Anda mendapatkan {value} dengan peluang {int(probability * 100)}%")
+            option['formatted_outcomes'] = formatted_outcomes  # List of outcomes for each option
+
+        return {
+            'random_options': random_options,
+        }
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Temukan opsi yang dipilih pemain berdasarkan nama
+        selected_option = next(
+            (option for option in Constants.options_data_price if option['name'] == player.selected_optionprice), None
+        )
+
+        # Lakukan drawing angka 1-100
+        if selected_option:
+            draw = random.randint(1, 100)
+
+            # Hitung hasil berdasarkan peluang
+            cumulative_probability = 0
+            for outcome, probability in selected_option['outcomes']:
+                cumulative_probability += probability * 100
+                if draw <= cumulative_probability:
+                    player.result_price = outcome
+                    player.endowment -= 25
+                    break
+
+    @staticmethod
+    def is_displayed(player: Player):
+        # Hanya tampilkan halaman ini jika pemain memilih "Iya"
+        return player.offer_accepted is not None and player.offer_accepted
+
+
+class PriceResults(Page):
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {
+            'result': player.result_price
+        }
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        player.endowment += player.result_price
+
+        # player.endowment += player.result_price1 + player.result_price2 +\
+        #                     player.result_price3 + player.result_price4 +\
+        #                     player.result_price5
+
+
+class RiskyOption_Allocation(Page):
+    form_model = 'player'
+    form_fields = ['allocation_invest', 'selected_optionallocation']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        # Mendapatkan pilihan acak (contoh daftar pilihan opsi)
+        random_options = random.sample(Constants.options_data_allocation, 5)
+
+        # Membuat teks yang terstruktur untuk setiap opsi dalam format list items
+        for i, option in enumerate(random_options):
+            option_outcomes = option['outcomes']
+            formatted_outcomes = []
+            for j, (value, probability) in enumerate(option_outcomes):
+                formatted_outcomes.append(f"Anda mendapatkan x{value} dengan peluang {int(probability * 100)}%")
+            option['formatted_outcomes'] = formatted_outcomes  # List of outcomes for each option
+
+        return {
+            'random_options': random_options,
+        }
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Temukan opsi yang dipilih pemain berdasarkan nama
+        selected_option = next(
+            (option for option in Constants.options_data_allocation if option['name'] == player.selected_optionallocation), None
+        )
+
+        # Lakukan drawing angka 1-100
+        if selected_option:
+            draw = random.randint(1, 100)
+
+            # Hitung hasil berdasarkan peluang
+            cumulative_probability = 0
+            for outcome, probability in selected_option['outcomes']:
+                cumulative_probability += probability * 100
+                if draw <= cumulative_probability:
+                    player.result_allocation = outcome
+                    player.endowment -= player.allocation_invest
+                    break
+
+    @staticmethod
+    def is_displayed(player: Player):
+        # Hanya tampilkan halaman ini jika pemain memilih "Iya"
+        return player.offer_accepted is not None and player.offer_accepted
+
+
+class AllocationResults(Page):
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {
+            'allocation': player.allocation_invest,
+            'result': player.result_allocation
+        }
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        player.endowment += player.allocation_invest * player.result_allocation
 
 
 class Confirmation_Cognitive_Task(Page):
@@ -187,5 +380,9 @@ class Results(Page):
         player.payoff = player.endowment
 
 
-page_sequence = [Intro, Game1, Game2, Confirmation_Cognitive_Task, Buytime, Game3, Game3_Results, Game4,
-                 Results]
+page_sequence = [Intro, BeforeGame1, RiskyOption_Price, PriceResults, BeforeGame2, RiskyOption_Allocation,
+                 AllocationResults, Confirmation_Cognitive_Task, Buytime, Game3, Game3_Results, Game4, Results]
+
+# page_sequence = [Intro, BeforeGame1, RiskyOption_Price1, RiskyOption_Price2, RiskyOption_Price3, RiskyOption_Price4,
+#                  RiskyOption_Price5, PriceResults, BeforeGame2, Game2, Confirmation_Cognitive_Task, Buytime, Game3,
+#                  Game3_Results, Game4, Results]
